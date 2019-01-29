cfbf20786d8f1a60c2f5d55315a0b943603452e6
Andrey Eremeev <andrey.eremeyev@gmail.com>
2019-01-29 18:36:37 +0400

    fix issues

--- ruby_basics/task_1/4_quadratic_equation.rb
+++ ruby_basics/task_1/4_quadratic_equation.rb
@@ -2,14 +2,15 @@
 # a * x * x + b * x + c = 0
 
 puts "Type a,b,c coefficients"
-a,b,c = gets.chomp.split(',').map(&:chomp).map(&:to_f)
+a, b, c = gets.chomp.split(',').map(&:chomp).map(&:to_f)
 
-d = b * b - 4 * a * c
+d = b**2 - 4 * a * c
 
 case true
 when d > 0
-  x1 = (-b + Math.sqrt(d)) / (2 * a)
-  x2 = (-b - Math.sqrt(d)) / (2 * a)
+  sqrt_of_d = Math.sqrt(d)
+  x1 = (-b + sqrt_of_d) / (2 * a)
+  x2 = (-b - sqrt_of_d / (2 * a)
   puts "Discriminant: #{d}"
   puts "x1 = #{x1}"
   puts "x2 = #{x2}"
