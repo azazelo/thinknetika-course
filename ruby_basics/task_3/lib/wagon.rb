ecf968b3923935315744b5f8915617cc66c9349c
Andrey Eremeev <andrey.eremeyev@gmail.com>
2019-03-01 17:41:29 +0400

    clean code

--- ruby_basics/task_3/lib/wagon.rb
+++ ruby_basics/task_3/lib/wagon.rb
@@ -19,17 +19,17 @@ class Wagon
     validate!
   end
 
-  def push(qty=1)
-    return you_trying_to_push_more_than_wagon_can_carry if qty > self.volume
-    return there_is_no_more_free_space_to_push if self.free_space.zero?
-    return free_space_is_too_small_for_this_qty if qty > self.free_space
+  def push(qty = 1)
+    return you_trying_to_push_more_than_wagon_can_carry if qty > volume
+    return there_is_no_more_free_space_to_push if free_space.zero?
+    return free_space_is_too_small_for_this_qty if qty > free_space
     @loaded += qty
     self
   end
 
-  def pull(qty=1)
-    return there_is_nothing_to_pull if self.loaded.zero?
-    return you_trying_to_pull_more_then_loaded if self.loaded < qty
+  def pull(qty = 1)
+    return there_is_nothing_to_pull if loaded.zero?
+    return you_trying_to_pull_more_then_loaded if loaded < qty
     @loaded -= qty
     self
   end
@@ -39,6 +39,6 @@ class Wagon
   end
 
   def info
-    "##{@number}, #{self.type}, всего: #{self.volume}, занято: #{self.loaded}, свободно: #{self.free_space}"
+    "##{@number}, #{type}, всего: #{volume}, занято: #{loaded}, свободно: #{free_space}"
   end
 end
