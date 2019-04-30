7a9af9e2cccfbfa04f9c674352c379b142146976
Andrey Eremeev <andrey.eremeyev@gmail.com>
2019-04-30 10:08:21 +0400

    fix locale

--- ror_basics/test-guru/config/routes.rb
+++ ror_basics/test-guru/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about/author', to: 'application#author'
-  get 'about(/:locale)', to: 'application#about'
+  get 'about', to: 'application#about'
 end
